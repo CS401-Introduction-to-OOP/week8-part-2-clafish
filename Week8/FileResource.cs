08f20779faf17e2072d41d468ef3a16595dfc693
clafish <andrew.korzh2008@gmail.com>
2026-03-28 15:32:25 +0200

    first stage finished

---
+++ Week8/FileResource.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1123.week8_part_2_clafish.Week8;
+
+public class FileResource : Resource, IDisposable
+{
+    public FileResource(string name) : base(name)
+    {
+    }
+    
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+
+    public override void Close()
+    {
+        IsOpen = false;
+    }
+
+    public void Dispose()
+    {
+        Close();
+    }
+}
---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1123.week8_part_2_clafish.Week8;
+
+public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+    public override void Close()
+    {
+        IsOpen = false;
+    }
+    public void Dispose()
+    {
+        Close();
+    }
+}
