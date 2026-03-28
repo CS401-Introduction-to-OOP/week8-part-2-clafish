fce5d64040af89df26605596f0252d618780daa7
clafish <andrew.korzh2008@gmail.com>
2026-03-28 16:18:25 +0200

    stage 2

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1123.week8_part_2_clafish.Week8;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    
+    public long GetTotalSize()
+    {
+        long sum = 0;
+        foreach (var item in _items)
+        {
+            sum += item.GetSize();
+        }
+
+        return sum;
+    }
+}
