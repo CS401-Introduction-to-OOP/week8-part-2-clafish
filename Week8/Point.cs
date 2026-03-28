dedaa8cb610201fab74750ac7cf556e0eb282843
clafish <andrew.korzh2008@gmail.com>
2026-03-28 15:52:04 +0200

    stage1

---
+++ Week8/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1123.week8_part_2_clafish.Week8;
+
+public struct Point
+{
+    public int X { get; set; }
+
+    public int Y { get; set; }
+    
+    public void Print()
+    {
+        Console.WriteLine($"X - {X}, Y - {Y}");
+    }
+}
