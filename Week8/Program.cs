9110ff3e6f2ec3996998e22d9553ca5ea2536dcd
clafish <andrew.korzh2008@gmail.com>
2026-03-28 16:47:17 +0200

    updated comments

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -8,13 +8,14 @@ class Program
         Point p2 = p1;
         p2.X = 10;
         
+        // struct is creating a ValueType, so when we copy, we create an independent object
         p1.Print();
         p2.Print();
-
-// Reference type demo
+        
         PointRef r1 = new PointRef { X = 1, Y = 2 };
         PointRef r2 = r1;
 
+        // class is creating a ReferenceType, so when we copy, we just copy pointer to memory
         r2.Y = 42;
         r1.Print();
         r2.Print();
