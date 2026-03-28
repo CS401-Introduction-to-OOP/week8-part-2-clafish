3cd27312fdd551ed91f37a70ad9c8214126f62f3
clafish <andrew.korzh2008@gmail.com>
2026-03-28 16:16:26 +0200

    stage 1

---
+++ Week8/DataItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1123.week8_part_2_clafish.Week8;
+
+public class DataItem
+{
+    public string Id { get; set; } = string.Empty;
+    public byte[] Payload { get; set; } = Array.Empty<byte>();
+    
+    public long GetSize()
+    {
+        return Payload.Length;
+    }
+}
